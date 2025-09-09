001b64bad1850f3869ded55f8b0170d9d9a53914
Iuri Mignoni <iurimig@gmail.com>
2025-09-09 18:59:32 -0300

    [feat]: update tag component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,61 +1,51 @@
-import type { IAlertProps } from "@/interfaces/IAlertProps";
-import { useRef } from "react";
+import type { IAlertProps } from '@/interfaces/IAlertProps';
+import { useRef } from 'react';
 import {
-  DialogActionTrigger,
-  DialogBody,
-  DialogCloseTrigger,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogRoot,
-} from "./ui/dialog";
-import { Button } from "./ui/button";
+    DialogActionTrigger,
+    DialogBody,
+    DialogCloseTrigger,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogRoot,
+} from './ui/dialog';
+import { Button } from './ui/button';
 
-export function Alert({
-  onCancel,
-  onConfirm,
-  isOpen,
-  title,
-  description = null,
-  isDeleteAction = true,
-}: IAlertProps) {
-  const cancelRef = useRef<HTMLButtonElement>(null);
+export function Alert({ onCancel, onConfirm, isOpen, title, description = null, isDeleteAction = true }: IAlertProps) {
+    const cancelRef = useRef<HTMLButtonElement>(null);
 
-  return (
-    <DialogRoot lazyMount open={isOpen}>
-      <DialogContent>
-        <DialogHeader fontWeight={"semibold"} fontSize={"md"}>
-          {title}
-        </DialogHeader>
-        {description && <DialogBody>{description}</DialogBody>}
-        <DialogFooter>
-          <DialogActionTrigger>
-            <Button
-              variant={"outline"}
-              ref={cancelRef}
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                onCancel();
-              }}
-            >
-              Cancelar
-            </Button>
-          </DialogActionTrigger>
-          <Button
-            colorPalette={isDeleteAction ? "red" : "blackAlpha"}
-            ml={3}
-            onClick={(e) => {
-              e.stopPropagation();
-              e.preventDefault();
-              onConfirm();
-            }}
-          >
-            Confirmar
-          </Button>
-        </DialogFooter>
-        <DialogCloseTrigger />
-      </DialogContent>
-    </DialogRoot>
-  );
+    return (
+        <DialogRoot lazyMount open={isOpen}>
+            <DialogContent>
+                <DialogHeader fontWeight={'semibold'} fontSize={'md'}>
+                    {title}
+                </DialogHeader>
+                {description && <DialogBody>{description}</DialogBody>}
+                <DialogFooter>
+                    <DialogActionTrigger
+                        ref={cancelRef}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            onCancel();
+                        }}
+                    >
+                        Cancelar
+                    </DialogActionTrigger>
+                    <Button
+                        colorPalette={isDeleteAction ? 'red' : 'blackAlpha'}
+                        ml={3}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            onConfirm();
+                        }}
+                    >
+                        Confirmar
+                    </Button>
+                </DialogFooter>
+                <DialogCloseTrigger />
+            </DialogContent>
+        </DialogRoot>
+    );
 }
